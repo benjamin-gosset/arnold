285eec8a09c1fccdc5c8bed8fd01eb5bd6434d3c
Benjamin <benjamin.gosset.pro@gmail.com>
2021-11-12 17:42:40 +0100

    Header and nav

--- functions.php
+++ functions.php
@@ -173,6 +173,48 @@ function arnold_override_yoast_breadcrumb_trail( $links ) {
 class Nav_Walker extends Walker_Nav_Menu 
 {
 	/**
+	 * Starts the list before the elements are added.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @see Walker::start_lvl()
+	 *
+	 * @param string   $output Used to append additional content (passed by reference).
+	 * @param int      $depth  Depth of menu item. Used for padding.
+	 * @param stdClass $args   An object of wp_nav_menu() arguments.
+	 */
+	public function start_lvl( &$output, $depth = 0, $args = null ) {
+		if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+			$t = '';
+			$n = '';
+		} else {
+			$t = "\t";
+			$n = "\n";
+		}
+		$indent = str_repeat( $t, $depth );
+
+		// Default class.
+		$classes = array( 'sub-menu' );
+
+		/**
+		 * Filters the CSS class(es) applied to a menu list element.
+		 *
+		 * @since 4.8.0
+		 *
+		 * @param string[] $classes Array of the CSS classes that are applied to the menu `<ul>` element.
+		 * @param stdClass $args    An object of `wp_nav_menu()` arguments.
+		 * @param int      $depth   Depth of menu item. Used for padding.
+		 */
+		$class_names = implode( ' ', apply_filters( 'nav_menu_submenu_css_class', $classes, $args, $depth ) );
+		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+		$output .= "{$n}{$indent}<svg width=\"14\" height=\"8\" viewBox=\"0 0 14 8\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">
+		<path d=\"M0.989502 1.70193L6.99991 7.71234L13.0103 1.70193L11.5961 0.28772L6.99991 4.88391L2.40372 0.28772L0.989502 1.70193Z\" fill=\"#2E3A59\"/>
+		</svg><ul$class_names>{$n}";
+	}
+
+
+	/**
 	 * Ends the element output, if needed.
 	 *
 	 * @since 3.0.0
@@ -205,9 +247,6 @@ class Nav_Walker extends Walker_Nav_Menu
 
 		if ( $item_has_children == true ) {
 			$output .= '<button aria-label="Voir le sous-menu"><span class="plus">&plus;</span><span class="minus">&minus;</span></button>';
-			$output .= '<svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M0.989502 1.70193L6.99991 7.71234L13.0103 1.70193L11.5961 0.28772L6.99991 4.88391L2.40372 0.28772L0.989502 1.70193Z" fill="#2E3A59"/>
-			</svg>';
 		}
 
 		$output .= '</li>'.$n;
